51c6b0785a9d8cd2eb8fe5c8d88244f6f096086d
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-08-31 18:48:35 +0530

    open file option added

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
-// const formidable = require("formidable");
-// const http = require("http");
+const express = require('express')
+const app = express()
+const port = 5000
 const path = require('path');
+const bodyParser = require('body-parser');
+const formidable = require("formidable")
+const multer = require('multer');
 const fs = require('fs');
-
-fs.writeFile('mynewfile2.txt', 'Hello content!', function (err) {
-	if (err) throw err;
-	console.log('Saved!');
+require('dotenv').config();
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads');
+     },
+    filename: function (req, file, cb) {
+        cb(null , file.originalname);
+    }
 });
+
+const upload = multer({storage : storage});
+
+
+//Middlewares
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine', 'ejs');
+
+
+//Routes
+app.get('/', function(req, res){
+  const data = {
+    textDocumentContent : " ",
+    textDocumentTitle: "Untitled",
+  }
+  res.render('index', data)
+})
+
+app.post('/openfile',upload.single('myfile'),(req,res)=>{ 
+  console.log(req.file);
+  let file = req.file;
+  let name = file.filename;
+  fs.readFile(`uploads/${name}`, (err, data) => {
+    if(err){
+      return res.status(500).json({mesage : "server gadbad"})
+    }
+   
+    res.status(200).json({
+      data : data.toString(),
+      name
+    });
+  })
+
+ 
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -50,3 +50,42 @@ function changeFont(val){
 function changeFontSize(val){
     content.style.fontSize = val +"px";
 }
+
+
+document.getElementById("openFile").onchange = async (e)=>{
+    let file = e.target.files;
+    if(file.length === 1){
+        if(file[0].type === "text/plain"){
+            await handleUpload(file[0]);                      
+        }else{
+            alert("Select only text file.")
+        }
+    }else{
+        alert("Select one file only");
+    }
+};
+
+function handleUpload(file){
+    var data = new FormData();
+    data.append("myfile", file);
+        
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            let result = JSON.parse(this.responseText);
+            content.innerText = result.data;
+            let name = result.name;
+            let newName = '';
+            for(let i=0; i<(name.length - 4); i++){
+                newName += name[i];
+            }
+            document.getElementById("document_name").innerText = newName;
+
+        }
+
+    });
+
+    xhr.open("POST", "/openfile");
+    xhr.send(data);
+}
